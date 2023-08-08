2c2c13bdd1b25d6bbd47bb5419055062d5fc9ccc
Vivian Zhao <vivianz1627@gmail.com>
2023-08-08 11:47:02 -0400

    Updates

--- pipeline/add_idx.py
+++
@@ -1,9 +0,0 @@
-# script that adds an 'idx' column to a csv
-
-import pandas as pd
-import sys
-
-input_file = sys.argv[1]
-df = pd.read_csv(input_file)
-df.index.name = 'idx'
-df.round(6).to_csv(input_file)
--- src/common.js
+++
@@ -1,5 +0,0 @@
-export const normalizeCategory = s => s.toLowerCase().replace(/[/ ]/g, '-');
-
-export function getAllCategories(taxonomy) {
-  return new Set(taxonomy.map(c => normalizeCategory(c.name)));
-}
